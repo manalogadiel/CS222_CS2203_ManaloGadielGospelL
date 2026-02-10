f01a45b34baaed6bb082092bf0d73a0c148f5b33
manalogadiel <gadielmanalo19@gmail.com>
2026-02-10 16:01:45 +0800

    upload

---
+++ ManaloGadielGospelL/Currency Converter/CurrencyConverter.cs
@@ -0,0 +1,12 @@
+using System;
+
+Console.Write("Enter amount in USD: ");
+double usd = double.Parse(Console.ReadLine());
+
+Console.Write("Enter exchange rate from USD to EUR: ");
+double rate = double.Parse(Console.ReadLine());
+
+double eur = usd * rate;
+
+Console.WriteLine($"Amount in EUR: {eur:F2}");
+
